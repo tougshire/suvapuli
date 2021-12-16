8da76d68935f6e980ab81a68d8fb3b34411418f9
tougshire <benjamin@tougshire.com>
2021-12-16 23:39:11 +0000

    Removed STATICFILES_DIRS from settings

--- settings.py
+++ settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-print ( BASE_DIR / 'static' )
-
-STATICFILES_DIRS = ( ( BASE_DIR / 'suvapuli/static' ), )
 
 STATIC_URL = env('STATIC_URL')
 
