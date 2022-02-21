3639ca0842384ea0645933ceabf354e749f1bf7e
tougshire <tougshire@tougshire.com>
2022-02-21 10:07:13 -0500

    PROJECT_INCLUDE_FILES['main_css']
    replaces PROJECT_CSS_FILE

--- settings.py
+++ settings.py
@@ -153,12 +153,11 @@ LIBTEKIN_ID_CHOICES = [
     ('essid', 'ESSID')
 ]
 
-PROJECT_INCLUDE_FILE = 'suvapuli/menu.html'
-# PROJECT_INCLUDE_FILES = {
-#     'main menu':'suvapuli/menu.html',
-#     'alt menu':'suvapuli/menu1.html',
-# }
-PROJECT_CSS_FILE = 'suvapuli/suvapuli.css'
+# PROJECT_INCLUDE_FILE = 'suvapuli/menu.html'
+PROJECT_INCLUDE_FILES = {
+    'main_menu':'suvapuli/menu.html',
+    'main_css':'suvapuli/suvapuli.css',
+}
 
 # SENDGRID_API_KEY = env('SENDGRID_API_KEY')
 # EMAIL_HOST = env('EMAIL_HOST')
