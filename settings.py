10af010849a672a8eaa0b13a57e172d07aa5f7ea
tougshire <tougshire@tougshire.com>
2022-06-03 08:08:37 -0400

    remove anymail

--- settings.py
+++ settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'anymail',
     'djmoney',
     'touglates.apps.TouglatesConfig',
     'tougshire_vistas.apps.TougshireVistasConfig',
